5b1565219c51284b1017733f3de97d28ded46091
Matteo Angelini <matteo.angelini@studenti.unipr.it>
2023-10-31 12:20:03 +0100

    FIX db

--- src/modules/todo/database/todo.repository.port.ts
+++ src/modules/todo/database/todo.repository.port.ts
@@ -2,5 +2,5 @@ import { RepositoryPort } from '@libs/ddd';
 import { TodoEntity } from '../domain/todo.entity';
 
 export interface TodoRepositoryPort extends RepositoryPort<TodoEntity> {
-  findByUser(userId: string): Promise<TodoEntity | null>;
+  findByUser(userId: string): Promise<TodoEntity[] | null>;
 }
--- src/modules/todo/database/todo.repository.ts
+++ src/modules/todo/database/todo.repository.ts
@@ -7,13 +7,14 @@ import { z } from 'zod';
 import { TodoStatus } from '../domain/todo.types';
 import { TodoEntity } from '../domain/todo.entity';
 import { TodoRepositoryPort } from './todo.repository.port';
+import { TodoMapper } from '../todo.mapper';
 
 export const todoSchema = z.object({
   id: z.string().min(1).max(255),
   createdAt: z.preprocess((val: any) => new Date(val), z.date()),
   updatedAt: z.preprocess((val: any) => new Date(val), z.date()),
   title: z.string(),
-  desciption: z.string(),
+  description: z.string(),
   status: z.nativeEnum(TodoStatus),
   userId: z.string().min(1).max(255),
 });
@@ -35,17 +36,20 @@ export class TodoRepository
   constructor(
     @InjectPool()
     pool: DatabasePool,
-    mapper: UserMapper,
+    mapper: TodoMapper,
     eventEmitter: EventEmitter2,
   ) {
     super(pool, mapper, eventEmitter, new Logger(TodoRepository.name));
   }
 
   async findByUser(userId: string): Promise<TodoEntity[]> {
-    const todos = await this.pool.many(
+    const records = await this.pool.many(
       sql.type(todoSchema)`SELECT * FROM "todos" WHERE userId = ${userId}`,
     );
 
-    return this.mapper.toDomain(todos);
+    const todos: TodoEntity[] = records.map((record) =>
+      this.mapper.toDomain(record),
+    );
+    return todos;
   }
 }
